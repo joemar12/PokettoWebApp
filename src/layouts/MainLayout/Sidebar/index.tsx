7de1292f99c0d98080a3df361453e05ce2378d31
Joe-Mar Gonzales <rameoj18@gmail.com>
2023-07-07 10:59:38 +0800

    put the sidebar on top of the z-axis

--- src/layouts/MainLayout/Sidebar/index.tsx
+++ src/layouts/MainLayout/Sidebar/index.tsx
@@ -18,7 +18,7 @@ const Siderbar = () => {
     <aside
       className={`${
         sidebarIsOpen ? "w-64 p-2" : "w-0 p-0 md:w-16 md:p-2"
-      } flex flex-col transition-[width] duration-200 bg-black md:relative absolute h-screen`}
+      } flex flex-col transition-[width] duration-200 bg-black md:relative absolute h-screen z-50`}
     >
       <div className={`overflow-y-auto overflow-x-hidden`}>
         <div className="h-24"></div>
